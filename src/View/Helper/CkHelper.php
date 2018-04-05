b43b747eb097936bd14373258b56974389579eef
Everton Barros Jr <geniunsDev@users.noreply.github.com>
2018-04-05 11:01:54 -0300

    Add the customization of distribution and layout
    Included the possibility of choose the version and toolbar layout, new way of use:
    On AppController or desired controller =>
        public $helpers = ['CkEditor.Ck' => [
    		'distribution' => 'full', // Default Option  / Other options => 'basic', 'standard'
                    'version' => 4.9.1     //Default option, others via CDN page 
        ]];

--- src/View/Helper/CkHelper.php
+++ src/View/Helper/CkHelper.php
@@ -2,11 +2,36 @@
 namespace CkEditor\View\Helper;
 
 use Cake\View\Helper;
+use Cake\View\View;
 
 class CkHelper extends Helper
 {
     public $helpers = ['Html', 'Form'];
 
+    /**
+     * Possibility of changing the distro and toolbar layout
+     * @param version The version of distro
+     * @param distribution The layout of distro i.e.: Basic, Full, Standard 
+     */
+    protected $_defaultConfig = [
+        'version' => '4.9.1',
+        'distribution' => 'full'
+      ];
+
+    protected $_configs = [];
+
+    /**
+     * Construct
+     * 
+     * @param View $view
+     * @param type $config
+     */
+    public function __construct(View $view, $config = []) {
+        parent::__construct($view, $config);
+        $this->_configs = $this->config();
+    }
+    
+    
     /**
     * Extention of the Form Helper to insert CKEditor for a form input
     *
@@ -14,28 +39,25 @@ class CkHelper extends Helper
     * @param array $options Options include $options['label'] for a custom label
     * @param array $ckEditorOptions This will pass any options from http://docs.ckeditor.com/#!/guide/dev_configuration to CKEditor
     * @param string $ckEditorUrl This gives an option to overwrite the CKEditor URL.  You can use a local URL then if required.
-    * @param  array $ckEditorPlugins An array of locally installed CKEditor plugins to load.
-    *         These should be passed as an array per plugin, in the format specified by
-    *         {@see https://docs.ckeditor.com/ckeditor4/latest/api/CKEDITOR_plugins.html#addExternal CKEditor documentation},
-    *         i.e. $plugins = [['sample', '/myplugins/sample/', 'my_plugin.js']]
     */
-    public function input($input, $options = [], $ckEditorOptions = [], $ckEditorUrl = null, $ckEditorPlugins = []) {
+    public function input($input, $options = [], $ckEditorOptions = [], $ckEditorUrl = null) {
         $lines = [];
 
+        /**
+         * Included the config passed before
+         */
         if (!$ckEditorUrl) {
-            $ckEditorUrl = '//cdn.ckeditor.com/4.6.1/standard/ckeditor.js';
+             $ckEditorUrl = "//cdn.ckeditor.com/{$this->_configs['version']}/{$this->_configs['distribution']}/ckeditor.js";
         }
 
         $lines[] = $this->Html->script($ckEditorUrl);
 
         $defaultOptions = ['type' => 'textarea', 'required' => false];
-
         $options = array_merge($defaultOptions, $options);
 
         $lines[] = $this->Form->error($input);
         $lines[] = $this->Form->input($input, $options);
-
-        $lines[] = $this->generateScript($input, $ckEditorOptions, $ckEditorPlugins);
+        $lines[] = $this->generateScript($input, $ckEditorOptions);
 
         return implode($lines, PHP_EOL);
     }
@@ -45,54 +67,10 @@ class CkHelper extends Helper
      * @param  string $input The name of the field, can be field_name or Model.field_name
      * @param  array  $options Any passed options, from http://docs.ckeditor.com/#!/guide/dev_configuration
      *         These should be passed as an array, e.g. ['fullPage' => true]
-     * @param  array $plugins An array of locally installed CKEditor plugins to load.
-     *         These should be passed as an array per plugin, in the format specified by
-     *         {@see https://docs.ckeditor.com/ckeditor4/latest/api/CKEDITOR_plugins.html#addExternal CKEditor documentation},
-     *         i.e. $plugins = [['sample', '/myplugins/sample/', 'my_plugin.js']]
      * @return string
      */
-    protected function generateScript($input, $options = [], $plugins = []) {
+    protected function generateScript($input, $options = []) {
         $script = '<script type="text/javascript">';
-
-        if (is_array($plugins) && !empty($plugins)) {
-
-          if (!key_exists('extraPlugins', $options)) {
-            $options['extraPlugins'] = '';
-          }
-
-          foreach ($plugins as $plugin_data) {
-
-            $extraPlugins = explode(',', $options['extraPlugins']);
-
-            $pluginIsAlreadyLoaded = FALSE;
-            foreach ($extraPlugins as $extraPlugin) {
-              if ($extraPlugin == $plugin_data[0]) {
-                $pluginIsAlreadyLoaded = TRUE;
-                break;
-              }
-            }
-
-            if (!$pluginIsAlreadyLoaded) {
-              if (strlen($options['extraPlugins']) != 0) {
-                $options['extraPlugins'] .= ',';
-              }
-              $options['extraPlugins'] .= $plugin_data[0];
-            }
-
-            $script .= 'CKEDITOR.plugins.addExternal(\'';
-            $script .= $plugin_data[0];
-            $script .= '\', \'';
-            $script .= $plugin_data[1];
-            $script .= '\', \'';
-
-            if (key_exists(2, $plugin_data)) {
-              $script .= $plugin_data[2];
-            }
-
-            $script .= '\');';
-          }
-        }
-
         $script .= 'CKEDITOR.replace(\'';
         $script .= $input;
         $script .= "'";
